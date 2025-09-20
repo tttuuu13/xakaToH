64039341d1054e819c9135c64d2a034d46b481b3
nick <11andriianovnikita@gmail.com>
2025-09-20 16:05:13 +0300

    cell

---
+++ HackatoN/Models/TextQuestionModel.swift
@@ -0,0 +1,12 @@
+//
+//  QuestionModel.swift
+//  HackatoN
+//
+//  Created by Nick on 20.09.2025.
+//
+
+struct TextQuestionModel {
+    var question: String
+    var answer: String?
+    var grade: Int?
+}
---
+++ HackatoN/QuestionCell.swift
@@ -0,0 +1,53 @@
+//
+//  CustomTableViewCell.swift
+//  HackatoN
+//
+//  Created by Nick on 20.09.2025.
+//
+
+import UIKit
+
+class QuestionCell: UITableViewCell {
+    let questionLabel: UILabel = {
+        let label = UILabel()
+        label.font = .systemFont(ofSize: 16, weight: .semibold)
+        label.numberOfLines = 0
+        return label
+    }()
+    
+    let answerTextView: UITextView = {
+        let textView = UITextView()
+        textView.font = .systemFont(ofSize: 15)
+        textView.isScrollEnabled = false
+        textView.layer.borderWidth = 1
+        textView.layer.borderColor = UIColor.lightGray.cgColor
+        textView.layer.cornerRadius = 8
+        return textView
+    }()
+    
+    override init(style: UITableViewCell.CellStyle, reuseIdentifier: String?) {
+        super.init(style: style, reuseIdentifier: reuseIdentifier)
+        
+        contentView.addSubview(questionLabel)
+        contentView.addSubview(answerTextView)
+        
+        questionLabel.pinTop(to: contentView, 12)
+        questionLabel.pinLeft(to: contentView, 16)
+        questionLabel.pinRight(to: contentView, 16)
+        
+        answerTextView.pinTop(to: questionLabel.bottomAnchor, 8)
+        answerTextView.pinLeft(to: contentView, 16)
+        answerTextView.pinRight(to: contentView, 16)
+        answerTextView.pinBottom(to: contentView, 12)
+        answerTextView.setHeight(mode: .grOE, 80)
+    }
+    
+    required init?(coder: NSCoder) {
+        fatalError("init(coder:) has not been implemented")
+    }
+    
+    func configure(with model: TextQuestionModel) {
+        questionLabel.text = model.question
+        answerTextView.text = model.answer ?? ""
+    }
+}
