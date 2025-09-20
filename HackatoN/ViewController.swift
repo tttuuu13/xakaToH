6da7f91be38bdc57071fa59be2dda96103c72e3c
Dmitriy <DreadDima12345@yandex.ru>
2025-09-20 15:27:45 +0300

    init

---
+++ HackatoN/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  HackatoN
+//
+//  Created by dread on 20.09.2025.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
